3679d01314240058eadb31ad45f9fa062707b7b1
Fernando Pinedo <fernando.pinedo@outlook.com>
2019-05-24 17:55:42 -0500

    Add computer 3d model

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,23 @@ import Layout from "../components/Layout";
 const Index = () => (
   <Layout>
     <p>Arquitectura de computadoras</p>
+
+    <div className="sketchfab-embed-wrapper">
+      <iframe width="1080" height="920"
+              src="https://sketchfab.com/models/82f78bbaf2d34f01af854a52151dbf49/embed?autospin=0.2&amp;autostart=1"
+              frameBorder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true"
+              webkitallowfullscreen="true"></iframe>
+
+      <p>
+        <a
+          href="https://sketchfab.com/3d-models/dream-computer-setup-82f78bbaf2d34f01af854a52151dbf49?utm_medium=embed&utm_source=website&utm_campaign=share-popup"
+          target="_blank" >Dream Computer Setup</a>
+        by <a href="https://sketchfab.com/SpanishPotato?utm_medium=embed&utm_source=website&utm_campaign=share-popup"
+              target="_blank" >Daniel Cardona</a>
+        on <a href="https://sketchfab.com?utm_medium=embed&utm_source=website&utm_campaign=share-popup" target="_blank"
+              >Sketchfab</a>
+      </p>
+    </div>
   </Layout>
 );
 
